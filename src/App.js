93c6b31927189f373f8059b1330ff38b7b89b3ff
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-07 21:25:28 +0900

    Organismの作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const user = {
+    name: "TK",
+    image: "https://source.unsplash.com/2l0CWTpcChI",
+    email: "12345@email.com",
+    phone: "090-1111-2222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "http://google.com"
+  };
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
