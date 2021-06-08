a427124f2da0b9cc524d504631c85a88b9ba91ba
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-08 11:15:03 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  const user = {
-    name: "TK",
-    image: "https://source.unsplash.com/2l0CWTpcChI",
-    email: "12345@email.com",
-    phone: "090-1111-2222",
-    company: {
-      name: "テスト株式会社"
-    },
-    website: "http://google.com"
-  };
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
